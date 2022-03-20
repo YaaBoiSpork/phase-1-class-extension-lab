f67c7d39ae7378a7a045af682c2bfb8b4cc2e6c3
Stephen Nerby <stephen.a.nerby@gmail.com>
2022-03-20 18:28:57 -0400

    commit tree 2c16a345323e5185dc0d9ca5c42da7f582f08ef5

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+class Polygon {
+    constructor(sidelengths) {
+        this.sidelengths = sidelengths
+    }
+
+    get countSides() {
+        return this.sidelengths.length
+    }
+
+    get perimeter() {
+        return this.sidelengths.reduce((value, newValue) => value + newValue)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        let [a, b, c] = this.sidelengths
+        if (a + b > c && b + c > a && c + a > b) {
+            return true
+        } else {
+            return false
+        }
+        
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        let [a, b, c, d] = this.sidelengths
+        if (a === b && b === c && c === d) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    get area() {
+        return this.sidelengths[0] * this.sidelengths[1]
+    }
+}
